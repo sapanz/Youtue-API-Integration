c10865acfd764161a1e622dc94bb8055d212a928
sapanz <spnzaveri@gmail.com>
2018-09-14 15:09:16 +0530

    Update youtube_search.py

--- youtube_search.py
+++ youtube_search.py
@@ -8,7 +8,7 @@ from googleapiclient.errors import HttpError
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = 'AIzaSyDm1VW8JqJ7F1Ghuzzs5Tl3rbdWKV2q0k4'
+DEVELOPER_KEY = 'AIzaSyDm1VW8JqJ7F1Ghuzzs5Tl3rXXXXXXXXXX'
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
