76d2b3a116473c83f218218ecfd106c6256a2e87
sapanz <spnzaveri@gmail.com>
2018-09-14 15:08:24 +0530

    Add files via upload

---
+++ youtube_search.py
@@ -0,0 +1,59 @@
+import argparse
+
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
+# tab of
+#   https://cloud.google.com/console
+# Please ensure that you have enabled the YouTube Data API for your project.
+DEVELOPER_KEY = 'AIzaSyDm1VW8JqJ7F1Ghuzzs5Tl3rbdWKV2q0k4'
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+
+def keyword_search(options):
+  youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+
+  # Call the search.list method to retrieve results matching the specified
+  # query term.
+  search_response = youtube.search().list(
+    q=options.q,
+    part='id,snippet',
+    maxResults=options.max_results
+  ).execute()
+
+  videos = []
+  channels = []
+  playlists = []
+
+  # Add each result to the appropriate list, and then display the lists of
+  # matching videos, channels, and playlists.
+  for search_result in search_response.get('items', []):
+    if search_result['id']['kind'] == 'youtube#video':
+      videos.append('%s (%s)' % (search_result['snippet']['title'],
+                                 search_result['id']['videoId']))
+    elif search_result['id']['kind'] == 'youtube#channel':
+      channels.append('%s (%s)' % (search_result['snippet']['title'],
+                                   search_result['id']['channelId']))
+    elif search_result['id']['kind'] == 'youtube#playlist':
+      playlists.append('%s (%s)' % (search_result['snippet']['title'],
+                                    search_result['id']['playlistId']))
+
+  print '\nVideos:\n', '\n'.join(videos), '\n'
+  print 'Channels:\n', '\n'.join(channels), '\n'
+  print 'Playlists:\n', '\n'.join(playlists), '\n'
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  keyword = raw_input("Enter the keyword to search : ")
+  parser.add_argument('--q', help='Search term', default=keyword)
+  parser.add_argument('--max-results', help='Max results', default=20)
+  args = parser.parse_args()
+
+  try:
+    keyword_search(args)
+  except HttpError, e:
+    print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
